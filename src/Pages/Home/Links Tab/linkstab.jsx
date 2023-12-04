6274e15fb4939c4b304c6befce9aaa40d8895e27
Sufyan Ahmed <121061255+SufyanAhmed1850@users.noreply.github.com>
2023-12-04 23:09:08 +0500

    Update linkstab.jsx

--- src/Pages/Home/Links Tab/linkstab.jsx
+++ src/Pages/Home/Links Tab/linkstab.jsx
@@ -5,6 +5,7 @@ import Linkscustomization from "../../../Components/Links Customization/linkscus
 import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { axiosPrivate } from "../../../api/axios.js";
+import axios from "axios";
 import { useContext } from "react";
 import linkContext from "../../../../context/linkContext.jsx";
 import useAuth from "../../../../hooks/useAuth.jsx";
@@ -41,7 +42,12 @@ const Linkstab = () => {
     useEffect(() => {
         (async () => {
             try {
-                const res = await axiosPrivate(getLinksEndpoint);
+                const res = await axios.get(
+                    "https://devlinks.cyclic.app/link",
+                    {
+                        withCredentials: true, // Include credentials with the request
+                    },
+                );
                 res?.data?.links && setLinksData(res.data.links);
                 res?.data?.links && setOrder(res.data.links.length + 1);
             } catch (error) {
@@ -107,7 +113,13 @@ const Linkstab = () => {
     const saveToDB = async () => {
         console.log(linksData);
         try {
-            const res = await axiosPrivate.post(saveLinksEndpoint, linksData);
+            const res = await axios.post(
+                "https://devlinks.cyclic.app/link/save",
+                linksData,
+                {
+                    withCredentials: true, // This line includes cookies with the request
+                },
+            );
             console.log(res);
             toast.success("Links saved successfully!", {
                 duration: 2000,
